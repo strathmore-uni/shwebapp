a38da156f2eb1347907782d760efec9d252bf2b7
Martin <martinmmbote@gmail.com>
2024-06-04 15:10:14 +0300

    ocr added

--- src/components/Webcamera.jsx
+++ src/components/Webcamera.jsx
@@ -1,5 +1,6 @@
-import React, { useRef, useState, useCallback } from 'react'
+import React, { useRef, useState, useEffect, useCallback } from 'react'
 import Webcam from 'react-webcam'
+import Tesseract from 'tesseract.js'
 import { parse } from 'mrz'
 
 const Webcamera = () => {
@@ -11,6 +12,21 @@ const Webcamera = () => {
     const imageSrc = webcamRef.current.getScreenshot();
     setImgSrc(imageSrc);
   }, [webcamRef, setImgSrc]);
+
+
+  const [recognizedText, setRecognizedText] = useState('');
+
+  useEffect(() => {
+    const recognizeText = async () => {
+      if (imgSrc) {
+        const result = await Tesseract.recognize(imgSrc);
+        setRecognizedText(result.data.text);
+      }
+    };
+    recognizeText();
+  }, [imgSrc]);
+
+  console.log(recognizedText);
 
   return (
     <div>
@@ -28,7 +44,12 @@ const Webcamera = () => {
             <img
               src={imgSrc}
             />
-          )}        
+          )}
+
+<div>
+      <h2 className='text-white'>{recognizedText}</h2>
+      <p>{recognizedText}</p>
+    </div>        
     </div>
   )
 }
