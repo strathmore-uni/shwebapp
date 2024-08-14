9c1ac6daca82fbfd1ab259ffac457b07af37d800
Martin <martinmmbote@gmail.com>
2024-08-14 09:06:34 +0300

    buttons

--- src/components/Sidepanel.jsx
+++ src/components/Sidepanel.jsx
@@ -6,7 +6,7 @@ const Sidepanel = () => {
     <div>
         <div className='w-[15vw] h-[40vw] border-black border-r-[0.15vw] flex justify-center pt-[1vw]'>
             <div>
-                <div className='w-[13vw] bg-black rounded-[0.3vw] h-[2.5vw] pt-[0.5vw] text-white flex pl-[0.6vw] gap-[0.7vw] text-[1.1vw] mb-[0.5vw]'>
+                <div className='w-[13vw] bg-black border-black border-[0.2vw] rounded-[0.3vw] h-[2.5vw] pt-[0.35vw] text-white flex pl-[0.6vw] gap-[0.6vw] text-[1.1vw] font-bold mb-[0.5vw]'>
                     <img src={homeWhite} className='h-[1.5vw]' />
 
                     <p>
@@ -14,14 +14,12 @@ const Sidepanel = () => {
                     </p>
                 </div>
 
-                <div className='w-[13vw] border-black border-[0.2vw] rounded-[0.3vw] h-[2.5vw] pt-[0.5vw] pl-[0.6vw] gap-[0.7vw] text-[1.1vw] mb-[0.5vw]'>
-                    <div className='absolute flex'>
-                        <img src={homeBlack} className='h-[1.5vw]' />
+                <div className='w-[13vw]  border-black border-[0.2vw] rounded-[0.3vw] h-[2.5vw] pt-[0.35vw] text-black flex pl-[0.6vw] gap-[0.6vw] text-[1.1vw] font-bold mb-[0.5vw]'>
+                    <img src={homeBlack} className='h-[1.5vw]' />
 
-                        <p>
-                            Dashboard
-                        </p>
-                    </div>
+                    <p>
+                        Dashboard
+                    </p>
                 </div>
             </div>
         </div>
