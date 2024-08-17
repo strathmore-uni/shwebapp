353afdc7e47f6a2f82d1e980837b97665a76bc2b
Martin <martinmmbote@gmail.com>
2024-08-18 01:14:19 +0300

    add users form

--- src/components/Addusers.jsx
+++ src/components/Addusers.jsx
@@ -1,28 +1,99 @@
 import React from 'react'
+import Select from 'react-select';
 
-const Addusers = () => {
+
+const Addusers = ({setShowUsersForm}) => {
+
+    const options = [
+        { value: 'guard', label: 'Guard' },
+        { value: 'receptionist', label: 'Receptionist' },
+      ];
+
+    const handleChange = (selectedOption) => {
+      console.log(`Selected:`, selectedOption);
+    };
+
+    const handleClick = () => {
+      setShowUsersForm(false);
+    };
+      
+
   return (
     <div>
-        <div className='w-screen h-screen bg-black bg-opacity-5 backdrop-blur-[0.1vw] absolute left-[0vw] top-[0vw] flex justify-end'>
-            <div className='w-[21vw] h-screen bg-white border-border-grey border-[0.2vw] rounded-[0.3vw] flex justify-center pt-[1.5vw]'>
+        <div className='w-screen h-screen absolute left-[0vw] top-[0vw] flex justify-end'>
+            <div className='w-screen h-screen bg-black bg-opacity-5 backdrop-blur-[0.1vw] absolute left-[0vw] top-[0vw] flex justify-end z-0'></div>
+
+            <div className='w-[21vw] h-screen bg-white border-border-grey border-[0.2vw] rounded-[0.3vw] flex justify-center pt-[1.5vw] z-10'>
                 <div>
                     <div>
-                        <p className='text-center'>
-                            Add New User
+                        <p className='text-center font-bold text-[1.2vw]'>
+                            ADD NEW USER
                         </p>
 
                         <div className='flex justify-center'>
-                            <div className='w-[7vw] h-[0.1vw] mt-[0.2vw] bg-background-grey'></div>
+                            <div className='w-[7vw] h-[0.1vw] mt-[0.1vw] mb-[1vw] bg-background-grey'></div>
                         </div>                    
                     </div>
 
                     <div>
                         <label>
-                            <p className='mb-[1vw]'>
-                                Enter phone number :
+                            <p className='mb-[0.2vw] ml-[0.3vw] text-[1vw]'>
+                                Enter Full Name :
                             </p>
-                            <input name="telnumber" type="number" placeholder='Phone number' className='text-black rounded-[0.3vw] text-[1vw] pl-[2vw] h-[2vw] w-[15vw] border-black border-[0.2vw]'  onChange={e => setphoneno(e.target.value)} />
+
+                            <input name="telnumber" type="number" placeholder='Enter Name' className='text-black rounded-[0.3vw] text-[1vw] pl-[0.5vw] h-[2vw] w-[16vw] border-black border-[0.2vw] mb-[0.9vw]'  onChange={e => setphoneno(e.target.value)} />
                         </label>
+
+                        <label>
+                            <p className='mb-[0.2vw] ml-[0.3vw] text-[1vw]'>
+                                Enter Staff ID :
+                            </p>
+
+                            <input name="telnumber" type="number" placeholder='Enter ID' className='text-black rounded-[0.3vw] text-[1vw] pl-[0.5vw] h-[2vw] w-[16vw] border-black border-[0.2vw] mb-[0.9vw]'  onChange={e => setphoneno(e.target.value)} />
+                        </label> 
+
+                        <label>
+                            <p className='mb-[0.2vw] ml-[0.3vw] text-[1vw]'>
+                                Enter Email Address :
+                            </p>
+
+                            <input name="telnumber" type="number" placeholder='Email Address' className='text-black rounded-[0.3vw] text-[1vw] pl-[0.5vw] h-[2vw] w-[16vw] border-black border-[0.2vw] mb-[0.9vw]'  onChange={e => setphoneno(e.target.value)} />
+                        </label> 
+
+                        <label>
+                            <p className='mb-[0.2vw] ml-[0.3vw] text-[1vw]'>
+                                Generate Password :
+                            </p>
+
+                            <input name="telnumber" type="number" placeholder='Email Address' className='text-black rounded-[0.3vw] text-[1vw] pl-[0.5vw] h-[2vw] w-[16vw] border-black border-[0.2vw] mb-[0.9vw]'  onChange={e => setphoneno(e.target.value)} />
+                        </label> 
+
+                        <p className='mb-[0.2vw] ml-[0.3vw] text-[1vw]'>
+                            Select Role :
+                        </p>
+
+                        <Select
+                            options={options}
+                            onChange={handleChange}
+                            placeholder="Choose a role"
+                            className='border-black border-[0.15vw] rounded-[0.4vw]'
+                        />                       
+                    </div>
+
+                    <div className='flex justify-center'>
+                        <div>
+                            <div className='w-[12vw] mt-[13.5vw] border-black bg-black text-white border-[0.15vw] rounded-[0.3vw] h-[2.5vw] flex justify-center text-[1.2vw] font-semibold cursor-pointer'>
+                                <p className='mt-[0.36vw]'>
+                                    Accept
+                                </p>
+                            </div>
+
+                            <div className='w-[12vw] mt-[0.7vw] border-black border-[0.15vw] rounded-[0.3vw] h-[2.5vw] flex justify-center text-[1.2vw] font-semibold cursor-pointer' onClick={handleClick}>
+                                <p className='mt-[0.36vw]'>
+                                    Cancel
+                                </p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { plusImage } from '../assets'
 import Table from './Table'
 import Datatable from './Datatable';
@@ -6,13 +6,17 @@ import Addusers from './Addusers';
 
 const Users = () => {
 
-    
+    const [showUsersForm, setShowUsersForm] = useState(false);
+
+    const handleClick = () => {
+      setShowUsersForm(true);
+    };
 
   return (
     <div>
         <div>
             <div className='flex justify-end w-[86vw]'>
-              <div className='w-[11vw] mt-[1vw] border-black border-[0.15vw] rounded-[0.3vw] h-[2.5vw] flex justify-center pl-[0.8vw] gap-[0.6vw] text-[1.2vw] font-semibold mb-[0.5vw] cursor-pointer'>
+              <div className='w-[11vw] mt-[1vw] border-black border-[0.15vw] rounded-[0.3vw] h-[2.5vw] flex justify-center pl-[0.8vw] gap-[0.6vw] text-[1.2vw] font-semibold mb-[0.5vw] cursor-pointer' onClick={handleClick}>
                   <img src={plusImage} className='h-[0.8vw] mt-[0.8vw]' />
 
                   <p className='mt-[0.36vw]'>
@@ -22,7 +26,10 @@ const Users = () => {
             </div>
             
             <Datatable />
-            <Addusers />
+
+            {showUsersForm && (
+              <Addusers setShowUsersForm={setShowUsersForm} />
+            )}
         </div>
     </div>
   )
