4b283b9676e8bd20afc1942261003efaba48fbf0
Martin <martinmmbote@gmail.com>
2024-09-18 20:19:45 +0300

    asdas

--- src/pages/Additions.jsx
+++ src/pages/Additions.jsx
@@ -9,11 +9,10 @@ import axios from 'axios';
 const Additions = () => {
 
     const [depart, setDepart] = useState('');
+    const [badge, setBadge] = useState('');
     const [departmentRefresh, setDepartmentRefresh] = useState(false);
-
     const [data, setData] = useState([]);
-
-    console.log(data);
+    const [backendBadges, setBackendBadges] = useState([]);
 
     const [filters, setFilters] = useState({
         global: { value: null, matchMode: FilterMatchMode.CONTAINS},
@@ -25,6 +24,13 @@ const Additions = () => {
         .then(data => setData(data))
         .catch(error => toast.error('Error fetching data:', error));
     }, [departmentRefresh]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/api/visitorsbadges')
+        .then(response => response.json())
+        .then(data => setBackendBadges(data))
+        .catch(error => toast.error('Error fetching data:', error));
+    }, []);
 
     const handleSubmit = async () => {
         if (!depart) {
@@ -49,7 +55,32 @@ const Additions = () => {
           console.error('Error submitting data:', error);
           toast.error('Failed to add Department');
         }
-      }; 
+      };
+      
+    const visitorBadgeSubmit = async () => {
+        if (!badge) {
+          toast.error("Badge name/number is required");
+          return;
+        }          
+
+        // Create an object with the state variables
+        const data = {
+            visitorsBadge: badge,          
+        };
+    
+        try {
+          // Send the data to the backend
+          const response = await axios.post('http://localhost:5000/api/visitorsbadges', data);       
+        //   console.log(response.data); // Log the response from the server
+          toast.success(badge + " has been successfully added");
+          setBadge('');          
+        //   setDepartmentRefresh(!departmentRefresh);
+
+        } catch (error) {
+          console.error('Error submitting data:', error);
+          toast.error('Failed to add Visitors Badge');
+        }
+      };
       
     const handleDelete = async (_id) => {
         try {
@@ -71,50 +102,100 @@ const Additions = () => {
     <div>
         <Toaster richColors />
 
-        <div className='mt-[1vw] ml-[3vw]'>
-            <div className='flex gap-[1vw]'>
-                <label>
-                    {/* <p className='mb-[0.2vw] ml-[0.3vw] text-[1vw]'>
-                        Add Department :
-                    </p> */}
+        <div className='mt-[2.5vw] ml-[3vw] flex gap-[4vw]'>
+            <div>
+                <div className='flex gap-[1vw]'>
+                    <label>
+                        {/* <p className='mb-[0.2vw] ml-[0.3vw] text-[1vw]'>
+                            Add Department :
+                        </p> */}
 
-                    <input type="text" placeholder='Enter Department Name' value={depart} className='text-black rounded-[0.3vw] text-[1vw] pl-[0.5vw] h-[2.5vw] w-[17vw] border-black border-[0.2vw] mb-[0.9vw]'  onChange={e => setDepart(e.target.value)} />
-                </label>
+                        <input type="text" placeholder='Enter Department Name' value={depart} className='text-black rounded-[0.3vw] text-[1vw] pl-[0.5vw] h-[2.5vw] w-[17vw] border-black border-[0.2vw] mb-[0.9vw]'  onChange={e => setDepart(e.target.value)} />
+                    </label>
 
-                <div className='w-[7vw] border-black bg-black text-white border-[0.15vw] rounded-[0.3vw] h-[2.5vw] flex justify-center text-[1.2vw] font-semibold cursor-pointer' onClick={handleSubmit}>
-                    <p className='mt-[0.36vw]'>
-                        ADD
-                    </p>
+                    <div className='w-[7vw] border-black bg-black text-white border-[0.15vw] rounded-[0.3vw] h-[2.5vw] flex justify-center text-[1.2vw] font-semibold cursor-pointer' onClick={handleSubmit}>
+                        <p className='mt-[0.36vw]'>
+                            ADD
+                        </p>
+                    </div>
+                </div>
+
+                <div className='w-[25vw] overflow-x-hidden border-black border-[0.2vw] rounded-[0.3vw] flex justify-center'>
+                    <div>
+                        <InputText 
+                            onInput={(e) =>
+                                setFilters({
+                                    global: { value: e.target.value, matchMode: FilterMatchMode.CONTAINS}
+                                })
+                            }
+                            
+                            placeholder='Search for Department'
+                            className='mt-[1vw] h-[2.5vw] rounded-[0.3vw] pl-[1.5vw] w-[20vw] bg-background-grey'
+                        />
+
+                        <DataTable className='w-[23vw] h-[30vw] mt-[1vw]' value={data} filters={filters} stripedRows placeholder='ji' rows={8}>
+                            <Column field="departmentName" header="Department" sortable />                   
+                            <Column
+                                header="Actions"
+                                body={(rowData) => (
+                                    <button
+                                        onClick={() => handleDelete(rowData._id)}
+                                        className="bg-red-500 text-white px-3 py-1 rounded hover:bg-red-700"
+                                    >
+                                        Delete
+                                    </button>
+                                )}
+                            />
+                        </DataTable>
+                    </div>
                 </div>
             </div>
 
-            <div className='w-[25vw] overflow-x-hidden border-black border-[0.2vw] rounded-[0.3vw] flex justify-center'>
-                <div>
-                    <InputText 
-                        onInput={(e) =>
-                            setFilters({
-                                global: { value: e.target.value, matchMode: FilterMatchMode.CONTAINS}
-                            })
-                        }
-                        
-                        placeholder='Search for Department'
-                        className='mt-[1vw] h-[2.5vw] rounded-[0.3vw] pl-[1.5vw] w-[20vw] bg-background-grey'
-                    />
+            <div>
+                <div className='flex gap-[1vw]'>
+                    <label>
+                        {/* <p className='mb-[0.2vw] ml-[0.3vw] text-[1vw]'>
+                            Add Department :
+                        </p> */}
 
-                    <DataTable className='w-[23vw] h-[30vw] mt-[1vw]' value={data} filters={filters} stripedRows placeholder='ji' rows={8}>
-                        <Column field="departmentName" header="Department" sortable />                   
-                        <Column
-                            header="Actions"
-                            body={(rowData) => (
-                                <button
-                                    onClick={() => handleDelete(rowData._id)}
-                                    className="bg-red-500 text-white px-3 py-1 rounded hover:bg-red-700"
-                                >
-                                    Delete
-                                </button>
-                            )}
+                        <input type="text" placeholder='Enter Vistor Badge' value={badge} className='text-black rounded-[0.3vw] text-[1vw] pl-[0.5vw] h-[2.5vw] w-[17vw] border-black border-[0.2vw] mb-[0.9vw]'  onChange={e => setBadge(e.target.value)} />
+                    </label>
+
+                    <div className='w-[7vw] border-black bg-black text-white border-[0.15vw] rounded-[0.3vw] h-[2.5vw] flex justify-center text-[1.2vw] font-semibold cursor-pointer' onClick={visitorBadgeSubmit}>
+                        <p className='mt-[0.36vw]'>
+                            ADD
+                        </p>
+                    </div>
+                </div>
+
+                <div className='w-[25vw] overflow-x-hidden border-black border-[0.2vw] rounded-[0.3vw] flex justify-center'>
+                    <div>
+                        <InputText 
+                            onInput={(e) =>
+                                setFilters({
+                                    global: { value: e.target.value, matchMode: FilterMatchMode.CONTAINS}
+                                })
+                            }
+                            
+                            placeholder='Search for Department'
+                            className='mt-[1vw] h-[2.5vw] rounded-[0.3vw] pl-[1.5vw] w-[20vw] bg-background-grey'
                         />
-                    </DataTable>
+
+                        <DataTable className='w-[23vw] h-[30vw] mt-[1vw]' value={backendBadges} filters={filters} stripedRows placeholder='ji' rows={8}>
+                            <Column field="visitorsBadge" header="Visitors Badge" sortable />                   
+                            {/* <Column
+                                header="Actions"
+                                body={(rowData) => (
+                                    <button
+                                        onClick={() => handleDelete(rowData._id)}
+                                        className="bg-red-500 text-white px-3 py-1 rounded hover:bg-red-700"
+                                    >
+                                        Delete
+                                    </button>
+                                )}
+                            /> */}
+                        </DataTable>
+                    </div>
                 </div>
             </div>
         </div>
