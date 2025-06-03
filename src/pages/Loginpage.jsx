96b8b1f4ece4a2c06d5e8b6aee7ecfe36019ee9f
Martin <martinmmbote@gmail.com>
2025-06-03 15:08:35 +0300

    test 5

--- src/pages/Loginpage.jsx
+++ src/pages/Loginpage.jsx
@@ -20,7 +20,7 @@ const Loginpage = () => {
                             <img src={fingerprintIcon} className='w-[25vw]' />
 
                             <p className='text-[3.5vw] font-normal text-white mt-[4vw]'>
-                                14.54 Build Test Hope
+                                15.07 ngrok
                             </p>
                         </div>
                     </Link>
