1531b44795004db059498ee73387b17479cf391e
Martin <martinmmbote@gmail.com>
2025-06-03 12:52:07 +0300

    test

--- src/pages/Loginpage.jsx
+++ src/pages/Loginpage.jsx
@@ -20,7 +20,7 @@ const Loginpage = () => {
                             <img src={fingerprintIcon} className='w-[25vw]' />
 
                             <p className='text-[3.5vw] font-normal text-white mt-[4vw]'>
-                                Scan fingerprint to Login
+                                Scan fingerprint -- replace with 
                             </p>
                         </div>
                     </Link>
