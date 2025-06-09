e94cbdbc9d741ae34d1528f491fb12127bd84057
Martin <martinmmbote@gmail.com>
2025-06-09 13:41:46 +0300

    removed some of the commented code

--- src/components/Webcamera.jsx
+++ src/components/Webcamera.jsx
@@ -1,11 +1,8 @@
 import React, { useRef, useState, useEffect, useCallback } from 'react'
 import Webcam from 'react-webcam'
 import { Link, useNavigate } from 'react-router-dom'
-// import Tesseract from 'tesseract.js'
-// import { createCanvas, loadImage } from 'canvas'
 import axios from 'axios'
-import Fillinfopage from '../pages/Fillinfopage'
-// import { parse } from 'mrz'
+// import Fillinfopage from '../pages/Fillinfopage'
 import { shutter } from '../assets'
 
 const Webcamera = ({ setSharedString, setiDname,myphone }) => {
@@ -20,31 +17,26 @@ const Webcamera = ({ setSharedString, setiDname,myphone }) => {
 
   const [animate, setAnimate] = useState(false);
 
-  const handleImageChange = (e) => {
-    setImage(e.target.files[0]);
-  };
+  // const handleImageChange = (e) => {
+  //   setImage(e.target.files[0]);
+  // };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const formData = new FormData();
-    formData.append('image', image);
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   const formData = new FormData();
+  //   formData.append('image', image);
 
-    try {
-      const response = await axios.post('https://5b96-2c0f-fe38-2190-a99f-392d-9920-6b27-3783.ngrok-free.app/ocr', formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data'
-        }
-      });
-      setResults(response.data);
-    } catch (error) {
-      console.error('Error uploading the image', error);
-    }
-  };
-
-  // const capture = React.useCallback(() => {
-  //   const imageSrc = webcamRef.current.getScreenshot();
-  //   setImgSrc(imageSrc);
-  // }, [webcamRef, setImgSrc]);
+  //   try {
+  //     const response = await axios.post('https://5b96-2c0f-fe38-2190-a99f-392d-9920-6b27-3783.ngrok-free.app/ocr', formData, {
+  //       headers: {
+  //         'Content-Type': 'multipart/form-data'
+  //       }
+  //     });
+  //     setResults(response.data);
+  //   } catch (error) {
+  //     console.error('Error uploading the image', error);
+  //   }
+  // };
 
   const capture = useCallback(async () => {
     const imageSrc = webcamRef.current.getScreenshot();
@@ -95,65 +87,7 @@ useEffect(() => {
 const navigateToNextPage = () => {
   history('/shwebapp/fill', { state: { id, name } });
 };
-  // const noName = [results[3], results[4]];
-  // console.log(results[2]);
-  
 
-  // const capture = async () => {
-  //   const imageSrc = webcamRef.current.getScreenshot();
-  //   const preprocessedImage = await preprocessImage(imageSrc);
-  //   setImgSrc(preprocessedImage);
-  // };
-
-  // const preprocessImage = async (imageSrc) => {
-  //   const img = await loadImage(imageSrc);
-  //   const canvas = createCanvas(img.width, img.height);
-  //   const ctx = canvas.getContext('2d');
-
-  //   ctx.drawImage(img, 0, 0);
-  //   // Convert to grayscale
-  //   const imageData = ctx.getImageData(0, 0, img.width, img.height);
-  //   const data = imageData.data;
-  //   // const contrast = 128; // Adjust contrast level here
-
-  //   // for (let i = 0; i < data.length; i += 4) {
-  //   //   const avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
-  //   //   const contrasted = ((avg - 128) * contrast / 128) + 128;
-  //   //   data[i] = contrasted; // red
-  //   //   data[i + 1] = contrasted; // green
-  //   //   data[i + 2] = contrasted; // blue
-  //   // }
-  //   // ctx.putImageData(imageData, 0, 0);
-
-  //   // Apply thresholding
-  //   const threshold = 110; // Adjust threshold level here
-  //   for (let i = 0; i < data.length; i += 4) {
-  //     const avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
-  //     const value = avg > threshold ? 255 : 0;
-  //     data[i] = value;     // red
-  //     data[i + 1] = value; // green
-  //     data[i + 2] = value; // blue
-  //   }
-
-  //   ctx.putImageData(imageData, 0, 0);
-
-  //   return canvas.toDataURL('image/jpeg');
-  // };
-
-
-  // const [recognizedText, setRecognizedText] = useState('');
-
-  // useEffect(() => {
-  //   const recognizeText = async () => {
-  //     if (imgSrc) {
-  //       const result = await Tesseract.recognize(imgSrc);
-  //       setRecognizedText(result.data.text);
-  //     }
-  //   };
-  //   recognizeText();
-  // }, [imgSrc]);
-
-  // console.log(recognizedText);
 
   return (
     <div>
@@ -192,31 +126,6 @@ const navigateToNextPage = () => {
           <img src={shutter} className={`w-[20vw] ml-[40vw] mt-[8vw] transition-transform duration-1000 ease-in-out
           ${animate ? 'scale-125 rotate-[360deg]' : ''}`} />        
         </button>
-          {/* {imgSrc && (
-            <img
-              src={imgSrc}
-            />
-          )} */}
-
-        {/* <div>
-          <h2 className='text-white'>{recognizedText}</h2>
-          <p>{recognizedText}</p>
-        </div>   */}
-
-        {/* <div className='text-white'>
-            {results.length > 0 && (
-              <div>    
-                <p>{myphone}</p>            
-                <p>
-                  {results[3][1]}
-                </p>
-
-                <p>
-                {results[4][1]}
-                </p>
-              </div>
-            )}
-        </div>   */}
         
         <div className='flex justify-center mt-[5vw]'>
         <button onClick={navigateToNextPage} className='border-[0.45vw] rounded-[1vw] text-center text-white py-[1vw] w-[40vw]'>
